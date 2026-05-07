22682c75286dd4fb599efee597e36ba2f7e9631a
Anonymous <rodriguezvazquezj09@gmail.com>
2026-05-07 03:58:44 -0400

    Delete js/pages/main.js

--- js/pages/main.js
+++
@@ -1,5 +0,0 @@
-// Inicialización general
-document.addEventListener('DOMContentLoaded', () => {
-  // Incluir toggle de tema en header si se necesita
-  // El componente header ya debe tener un botón con id="theme-toggle"
-});
