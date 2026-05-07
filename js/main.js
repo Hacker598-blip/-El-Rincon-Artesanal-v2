322e45867c20a180f103bd126651d0ee8344cdab
Anonymous <rodriguezvazquezj09@gmail.com>
2026-05-07 03:59:11 -0400

    Commit
    Commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Inicialización general
+document.addEventListener('DOMContentLoaded', () => {
+  // Incluir toggle de tema en header si se necesita
+  // El componente header ya debe tener un botón con id="theme-toggle"
+});
